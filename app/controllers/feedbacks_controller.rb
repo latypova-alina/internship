0a90e086b202b84172b0983de21f79fcbd7d18d3
latypova-alina <alinkin9696@mail.ru>
2017-10-15 16:31:04 +0300

    add prefilled fields

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,8 +1,10 @@
 class FeedbacksController < ApplicationController
-  expose :feedback, attributes: :feedback_params
+  expose_decorated :feedback, attributes: :feedback_params
 
   def create
     feedback.save
+    flash[:notice] = "Feedback was successfully send!"
+    redirect_to root_url
     FeedbackMailer.feedback_email.deliver_later
   end
 
