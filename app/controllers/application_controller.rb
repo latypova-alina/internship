a518f8cddedf635c85e2a308602a875d5bfa67a0
latypova-alina <alinkin9696@mail.ru>
2017-10-16 13:00:04 +0300

    change the alert message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   private
 
   def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
+    flash[:alert] = "You are not admin! >___>"
     redirect_to root_path
   end
 end
