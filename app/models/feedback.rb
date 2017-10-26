913aeb996660ed6c6e6b9fa574326318e2c6fb32
latypova-alina <alinkin9696@mail.ru>
2017-10-27 00:39:56 +0300

    create feedback and user relations

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,4 @@
 class Feedback < ApplicationRecord
   validates :email, :text, presence: true
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable,
     :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :feedbacks
   validates :full_name, presence: true
 end
