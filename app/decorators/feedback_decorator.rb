6195f880435c538dce28cc0e99a23bcf868659fd
latypova-alina <alinkin9696@mail.ru>
2017-10-27 00:53:55 +0300

    only new feedbacks have pre-filled fields of current_user

--- app/decorators/feedback_decorator.rb
+++ app/decorators/feedback_decorator.rb
@@ -1,11 +1,11 @@
 class FeedbackDecorator < ApplicationDecorator
   delegate_all
 
-  def feedback_sender_name
+  def new_feedback_sender_name
     h.current_user.full_name if h.user_signed_in?
   end
 
-  def feedback_sender_email
+  def new_feedback_sender_email
     h.current_user.email if h.user_signed_in?
   end
 end
